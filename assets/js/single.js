0fba0d7ac2e964bfbbb7e2186f521671d19ab5f6
Esteban Romero <esroleo@gmail.com>
2021-01-12 13:56:50 -0500

    added a link for more than 30 records

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,23 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
 
+// Warning message of more than 30 issues. Paginated issues
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+  };
+
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -12,6 +29,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
           response.json().then(function(data) {
             displayIssues(data);
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
           });
         }
         else {
@@ -61,7 +82,7 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("esroleo/html-css-git-challenge1-code-refactor");
+getRepoIssues("facebook/react");
 
 
 
